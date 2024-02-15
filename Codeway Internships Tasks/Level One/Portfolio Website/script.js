580e062e890b7e226409498ed27a343e496033c6
Sarvesh Deshmukh <128734690+Sarveshsdeshmukh@users.noreply.github.com>
2024-02-15 22:10:26 +0530

    Add files via upload
    Level One All Tasks are Uploaded.

---
+++ Codeway Internships Tasks/Level One/Portfolio Website/script.js
@@ -0,0 +1,19 @@
+const locoScroll = new LocomotiveScroll({
+    el: document.querySelector("#main"),
+    smooth: true
+  });
+
+  
+
+
+let timeline = gsap.timeline();
+  
+// timeline.from('nav ul li,nav img',{
+//   opacity : 0,
+//   scale :0,
+//   // y : -50,
+//   duration : 1,
+//   delay : 0.2,
+//   stagger : 0.2
+// })
+
